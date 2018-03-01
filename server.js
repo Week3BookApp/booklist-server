e0e03e63a07666e26da743b8e71fa6edd6bd272d
Steven Starwalt <steven.starwalt@yahoo.com>
2018-02-28 22:31:53 -0800

    couldn't fix the final bug

--- server.js
+++ server.js
@@ -16,12 +16,18 @@ app.use(cors()); // Allows access from multiple soirces with no validation.
 
 // Sets up out first query to get our book data
 app.get('/api/v1/books', (request, response) => {
-  client.query(`SELECT book_id, title, author, image_url, isbn, description FROM books;`)
+  client.query(`SELECT book_id, title, author, image_url, isbn FROM books;`)
     .then(results => response.send(results.rows))
     .catch(console.log);
 });
 
+// app.get('/api/v1/books/:id', (request, response) => {
+//   client.query(`SELECT * FROM books WHERE id=${request.params.id};`)
+//     .then(results => response.send(results.rows))
+//     .catch(console.log);
+// });
+
 app.get('*', (request, response) => response.redirect(CLIENT_URL)); // Catch all for any other route to redirect to the home page.
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
 
-app.post()
+// app.post();
