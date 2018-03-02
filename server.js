f682a182e69e74b3a5732377db4cafdc9502b0a8
Steven Starwalt <steven.starwalt@yahoo.com>
2018-03-01 18:33:43 -0800

    fixed the querys for push, put and delete

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const cors = require('cors');
 const pg = require('pg');
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser').urlencoded({extended: true });
 
 const app = express();
 const PORT = process.env.PORT;
@@ -12,9 +12,8 @@ const CLIENT_URL = process.env.CLIENT_URL;
 const client = new pg.Client(process.env.DATABASE_URL);
 client.connect();
 
-app.use(cors()); // Allows access from multiple sources with no validation.
+app.use(cors()); 
 
-// Sets up out first query to get our book data
 app.get('/api/v1/books', (request, response) => {
   client.query(`SELECT * FROM books;`)
     .then(results => response.send(results.rows))
@@ -28,7 +27,8 @@ app.get('/api/v1/books/:id', (request, response) => {
 });
 
 app.post('/api/v1/books/', bodyParser, (request, response) => {
-  client.query(`INSERT INTO books(title, author, isbn, image_url, description)VALUES($1, $2, $3, $4, $5);`
+  console.log('inside post');
+  client.query(`INSERT INTO books(title, author, isbn, image_url, description)VALUES($1, $2, $3, $4, $5);`,
     [
       request.body.title,
       request.body.author,
@@ -40,8 +40,8 @@ app.post('/api/v1/books/', bodyParser, (request, response) => {
     .catch(console.error);
 });
 
-app.put('/api/v1/books/:id', bodyParser, (request, response) => { //added in lab 13 for book update
-  client.query(`UPDATE books SET title=$1, author=$2, isbn=$3, image_url=$4, description=$5 WHERE book_id=$6);`
+app.put('/api/v1/books/:id', bodyParser, (request, response) => {
+  client.query(`UPDATE books SET title=$1, author=$2, isbn=$3, image_url=$4, description=$5 WHERE book_id=$6);`,
     [
       request.body.title,
       request.body.author,
@@ -54,11 +54,11 @@ app.put('/api/v1/books/:id', bodyParser, (request, response) => { //added in lab
     .catch(console.error);
 });
 
-app.delete('/api/v1/books/:id', (request, response) =>{ //added in lab 13 for book delete
+app.delete('/api/v1/books/:id', (request, response) =>{ 
   client.query(`DELETE FROM books WHERE id=${request.params.id};`)
     .then(() => response.send('Delete complete'))
     .catch(console.error);
 });
 
-app.get('*', (request, response) => response.redirect(CLIENT_URL)); // Catch all for any other route to redirect to the home page.
+app.get('*', (request, response) => response.redirect(CLIENT_URL));
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
